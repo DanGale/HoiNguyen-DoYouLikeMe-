88d211dd967890c806d8583c76ba29294f9dc9cd
DanGale <100851969+DanGale@users.noreply.github.com>
2022-12-21 02:50:58 +0700

    First commit

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ yesButton.addEventListener('click', () => {
     message.innerHTML = '<img class="tym-gif" src="images/luvutoo.gif" alt=""><div>I like you too<3</div>';
 });
 
-noButton.addEventListener('click', () => {
+noButton.addEventListener('mouseover', () => {
     const x = Math.floor(Math.random() * 80) + 1;
     const y = Math.floor(Math.random() * 90) + 1;
     noButton.setAttribute("style", `position: absolute; top:${x}%;left: ${y}%;`)
