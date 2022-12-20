011a3c1489e93c5aa04bec543658f8a5bba25cb1
DanGale <100851969+DanGale@users.noreply.github.com>
2022-12-21 02:09:18 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const yesButton = document.getElementById('yes-button');
+const noButton = document.getElementById('no-button');
+const message = document.getElementById('message');
+
+yesButton.addEventListener('click', () => {
+
+    message.innerHTML = '<img class="tym-gif" src="images/luvutoo.gif" alt=""><div>I like you too<3</div>';
+});
+
+noButton.addEventListener('click', () => {
+    const x = Math.floor(Math.random() * 80) + 1;
+    const y = Math.floor(Math.random() * 90) + 1;
+    noButton.setAttribute("style", `position: absolute; top:${x}%;left: ${y}%;`)
+});
+
+
